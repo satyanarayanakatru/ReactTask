968313ee038c9a66d863f680c6b38e1e94e0aed3
satyanarayanakatru <satyanarayanayadav50@gmail.com>
2026-05-15 18:24:24 +0530

    Task - 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SelfIntroduction from './SelfIntroduction'
+import Parent from './Parent'
 
 export default function App() {
   return (
     <div>
-      <SelfIntroduction/>
+      {/* <SelfIntroduction/> */}
+      <Parent/>
     </div>
   )
 }
